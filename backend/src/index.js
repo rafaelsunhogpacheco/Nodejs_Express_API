eb4b0d4ee2a128e53c32f28296e940881ab24da9
dev-rafa1707 <rafa1707@gmail.com>
2023-03-22 23:56:37 -0300

    add rota get

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,14 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 3000
+
+
+app.get('/',(req,res)=>{
+    res.status(200).send('Hello World')
+})
+
+
 
 app.listen(PORT,()=>{
     console.log('Listening on port '+PORT)
