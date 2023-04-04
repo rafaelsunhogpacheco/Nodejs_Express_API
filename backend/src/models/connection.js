814157fb0f11d4e391bb0d2133e8eb43ee6256e2
dev-rafa1707 <rafa1707@gmail.com>
2023-04-03 21:45:34 -0300

    corrige coneection.js

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,33 +1,33 @@
 
-// const mysql = require('mysql2/promise')
+const mysql = require('mysql2/promise')
 
 require('dotenv').config()
 
-// const connection = mysql.createPool({
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DB
+})
+
+module.exports = connection
+
+
+// const Sequelize = require("sequelize")
+
+// const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
 //     host: process.env.MYSQL_HOST,
-//     user: process.env.MYSQL_USER,
-//     password: process.env.MYSQL_PASSWORD,
-//     database:process.env.MYSQL_DB
+//     dialect: "mysql"
 // })
 
-// module.exports = connection
+// sequelize.authenticate()
+// .then(function(){
+//     console.log("Conexao com DB realizada com sucesso")
+// }).catch(function(){
+//     console.log("Erro conexao com DB não realizada com sucesso")
+// })
 
-
-const Sequelize = require("sequelize")
-
-const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER,process.env.MYSQL_PASSWORD, {
-    host: process.env.MYSQL_HOST,
-    dialect: "mysql"
-})
-
-sequelize.authenticate()
-.then(function(){
-    console.log("Conexao com DB realizada com sucesso")
-}).catch(function(){
-    console.log("Erro conexao com DB não realizada com sucesso")
-})
-
-module.exports = sequelize 
+// module.exports = sequelize 
 
 
 
