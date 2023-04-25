d391135a600b924a321b292bfd4731164110d7ed
dev-rafa1707 <rafa1707@gmail.com>
2023-04-25 12:23:57 -0300

    complementa info getAll

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -8,7 +8,7 @@ const routeName = "product" ;
 // CREATE - Cria um novo produto
 router.post(`/${routeName}`, productController.create);
 
-// READ - Retorna todos os produtos
+// READ ALL - Retorna todos os produtos
 router.get(`/${routeName}`, productController.getAll);
 
 // READ - Retorna um produto por ID
